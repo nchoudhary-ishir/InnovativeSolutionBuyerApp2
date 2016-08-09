71587eb42c172e0a38b5a50b9eeba898f4997a68
nchoudhary-ishir <nchoudhary@ishir.com>
2016-08-09 20:34:59 +0530

    Changed home page

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,6 +1,7 @@
 angular.module('orderCloud')
 	.config(HomeConfig)
 	.controller('HomeCtrl', HomeController)
+
 ;
 
 function HomeConfig($stateProvider) {
@@ -10,11 +11,20 @@ function HomeConfig($stateProvider) {
 			url: '/home',
 			templateUrl: 'home/templates/home.tpl.html',
 			controller: 'HomeCtrl',
-			controllerAs: 'home'
+			controllerAs: 'home',
+			resolve: {
+				Parameters: function($stateParams, OrderCloudParameters) {
+					return OrderCloudParameters.Get($stateParams);
+				},
+				ProductList : function(OrderCloud, Parameters) {
+					return OrderCloud.Me.ListProducts(Parameters.search, Parameters.page, Parameters.pageSize || 12, Parameters.searchOn, Parameters.sortBy, Parameters.filters,Parameters.from, Parameters.to);
+				}
+			}
 		})
 	;
 }
 
-function HomeController() {
+function HomeController(ProductList) {
 	var vm = this;
+	vm.list=ProductList;
 }
